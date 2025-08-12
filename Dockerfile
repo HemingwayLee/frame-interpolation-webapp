2f5a06bce972b90180fca8a3e5f045c6eaf3ffaf
HemingwayLee <coherence0815@gmail.com>
2025-08-13 00:50:05 +0900

    fix bug

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,17 @@
 FROM python:3.9.23
 
 RUN apt-get update && apt-get install -y net-tools postgresql-client curl sudo
-RUN curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
-RUN apt-get install -y nodejs
+RUN curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+RUN apt-get update && apt-get install -y nodejs
 RUN apt-get install -y ffmpeg libgl1
-RUN apt-get install -y npm
 
 RUN mkdir -p /home/app/
 COPY . /home/app/
 
-
 WORKDIR /home/app/frontend/
+RUN npm install --save-dev @babel/plugin-proposal-private-property-in-object
 RUN npm install .
 
 WORKDIR /home/app/backend/
 RUN pip3 install -r requirements.txt
-
-WORKDIR /home/app/frontend/
-RUN npm install --save-dev @babel/plugin-proposal-private-property-in-object
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,62 +3,34 @@ import React from 'react';
 import moment from 'moment';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import {
-  Card,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
-  TablePagination
-} from '@mui/material';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import intl from 'react-intl-universal';
+
+const Drawer = styled(MuiDrawer, {})(
+  ({}) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: 360,
+      boxSizing: 'border-box'
+    },
+  }),
+);
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: "#000000" /*or whatever color you desire */
+    }
+  },
+});
 
 export default function Dashboard() {
-  const [personList, setPersonList] = React.useState([]);
-  const [personCount, setPersonCount] = React.useState(0);
-  const [controller, setController] = React.useState({
-    page: 0,
-    rowsPerPage: 10
-  });
-
-  React.useEffect(() => {
-    getData();
-  }, [controller]);
-
-  const getData = async () => {
-    const url = `/api/show/page/${controller.page}/size/${controller.rowsPerPage}/`
-    try {
-      const response = await fetch(url);
-      if (response.statusText === 'OK') {
-        const data = await response.json();
-        console.log(data);
-        setPersonList(data.data);
-        setPersonCount(data.total);
-      } else {
-        throw new Error('Request failed')
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handlePageChange = (event, newPage) => {
-    setController({
-      ...controller,
-      page: newPage
-    });
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setController({
-      ...controller,
-      rowsPerPage: parseInt(event.target.value, 10),
-      page: 0
-    });
-  };
-
   const doInsert = () => {
     fetch('/api/insert/', {
       method: 'GET',
@@ -67,17 +39,36 @@ export default function Dashboard() {
       return response.json();
     })
     .then(function(myJson) {
-      getData();
+      alert(myJson["result"]);
+    });
+  }
+  
+  const showPerson = () => {
+    fetch('/api/show/', {
+      method: 'GET',
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
       alert(myJson["result"]);
     });
   }
   
   return (
+    <ThemeProvider theme={theme}>
       <Box sx={{ display: 'flex' }}>
+        <Drawer variant="permanent">
+          <List component="nav">
+          </List>
+        </Drawer>
         <Box
           component="main"
           sx={{
-            backgroundColor: '#AAAAAA',
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
             flexGrow: 1,
             height: '100vh',
             overflow: 'auto',
@@ -87,52 +78,7 @@ export default function Dashboard() {
             <Grid container spacing={3}>
               <Grid item xs={12}>
                 <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Card>
-                    <Table>
-                      <TableHead>
-                        <TableRow>
-                          <TableCell>
-                            Id
-                          </TableCell>
-                          <TableCell>
-                            First Name
-                          </TableCell>
-                          <TableCell>
-                            Last Name
-                          </TableCell>
-                          <TableCell>
-                            Age
-                          </TableCell>
-                        </TableRow>
-                      </TableHead>
-                      <TableBody>
-                        {personList.map((person) => (
-                          <TableRow key={person.id}>
-                            <TableCell>
-                              {person.id}
-                            </TableCell>
-                            <TableCell>
-                              {person.fname}
-                            </TableCell>
-                            <TableCell>
-                              {person.lname}
-                            </TableCell>
-                            <TableCell>
-                              {person.age}
-                            </TableCell>
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                    <TablePagination
-                      component="div"
-                      onPageChange={handlePageChange}
-                      page={controller.page}
-                      count={personCount}
-                      rowsPerPage={controller.rowsPerPage}
-                      onRowsPerPageChange={handleChangeRowsPerPage}
-                    />
-                  </Card>
+                  <Button variant="contained" component="label" onClick={showPerson}>Show Person</Button>
                 </Paper>
               </Grid>
               <Grid item xs={12}>
@@ -144,5 +90,6 @@ export default function Dashboard() {
           </Container>
         </Box>
       </Box>
+    </ThemeProvider>
   );
 }
